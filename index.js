c304edc094d91420e9da49d149c61ed9a390e870
Shyam Singh <2021pietcsshyam165@poornima.org>
2024-09-25 19:07:08 +0530

    Ignore node_modules folder

---
+++ index.js
@@ -0,0 +1,5 @@
+//common js module of second.js ---> as we are importing some data from the second.js file
+
+const lovish = require("./second");
+
+console.log("hello world", lovish)
---
+++ second.js
@@ -0,0 +1,11 @@
+//(function(exports, require, module, __filename, __dirname){
+
+shyam = {
+    name: "Shyam",
+    favNum: 3, 
+    developer: true
+}
+console.log(exports, require, module, __filename, __dirname)
+module.exports = shyam;
+
+// })
